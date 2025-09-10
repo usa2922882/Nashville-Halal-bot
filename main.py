3b296ba3d0c17026a03114b273fd82ab152fb860
usa2922882 <usa2922882@gmail.com>
2025-09-09 21:52:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
+from telegram.ext import ApplicationBuilder
+application = ApplicationBuilder().token("8210249353:AAFvDJZgX2O-aSVLaxKjbp9Sg0J3pT770uo").build()
 print("Bot ishga tushdi ✅")
 application.run_polling()
