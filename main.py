6d2bdac3aeeda59a825e3fad5e5b69a4d8247039
usa2922882 <usa2922882@gmail.com>
2025-09-09 23:19:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from telegram.ext import ApplicationBuilder
-application = ApplicationBuilder().token("8210249353:AAFvDJZgX2O-aSVLaxKjbp9Sg0J3pT770uo").build()
-print("Bot ishga tushdi ✅")
-application.run_polling()
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+import os
+
+BOT_TOKEN = "8210249353:AAFvDJZgX2O-aSVLaxKjbp9Sg0J3pT770uo"
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Assalomu alaykum! Bot ishga tushdi ✅")
+
+if __name__ == '__main__':
+    print("Bot ishga tushyapti...")
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
