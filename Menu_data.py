ddbc4e66847bba5a27a23315911708cabae72de7
usa2922882 <usa2922882@gmail.com>
2025-09-11 10:21:39 -0500

    Create Menu_data.py

---
+++ Menu_data.py
@@ -0,0 +1,12 @@
+
+MENU_CATEGORIES = {
+    "1. Suyuq Taomlar": ["Sho'rva", "Mastava", "Lag'mon"],
+    "2. Quyuq Taomlar": ["Palov", "Kabob", "Qozon Kabob"],
+    "3. Ichimliklar": ["Choy", "Coca Cola", "Sharbat"]
+}
+
+PRICES = {
+    "Sho'rva": 5, "Mastava": 6, "Lag'mon": 7,
+    "Palov": 10, "Kabob": 12, "Qozon Kabob": 13,
+    "Choy": 1, "Coca Cola": 2, "Sharbat": 3
+}
